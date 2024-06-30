3f87d19dc770172b3a1382722885ea1b18ea9d67
Olle Brask <ollstah@gmail.com>
2024-06-30 23:45:37 +0000

    Add Switch and Routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,38 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.jpeg";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
+        <NavLink to="/">
         <Navbar.Brand>
           <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        </Navbar.Brand></NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/">
                 <i className="fa-solid fa-house">Home</i>
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/login">
                 <i className="fa-solid fa-right-to-bracket">Log in</i>
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/signup">
                 <i className="fa-solid fa-user-plus">Sign up</i>
-            </Nav.Link>
+            </NavLink>
            
           </Nav>
         </Navbar.Collapse>
