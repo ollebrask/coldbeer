3030f015166f4028d0d1ea6c52806dd5aee9f497
Olle Brask <ollstah@gmail.com>
2024-07-08 22:27:25 +0000

    Add CommentEditForm and function

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,59 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import { Media } from "react-bootstrap";
 import Avatar from "../../components/Avatar";
 import styles from "../../styles/Comment.module.css";
 import { MoreDropdown } from "../../components/MoreDropdown";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosRes } from "../../api/axiosDefaults";
+import CommentEditForm from "./CommentEditForm";
 
 const Comment = (props) => {
-    const {
-        profile_image,
-        owner,
-        updated_at,
-        content,
-        id,
-        setPost,
-        setComments,
-      } = props;
+  const {
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPost,
+    setComments,
+  } = props;
 
-      const currentUser = useCurrentUser();
-      const is_owner = currentUser?.username === owner;
+  const [showEditForm, setShowEditForm] = useState(false);
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
 
-      const handleDelete = async () => {
-        try {
-          await axiosRes.delete(`/comments/${id}/`);
-          setPost((prevPost) => ({
-            results: [
-              {
-                ...prevPost.results[0],
-                comments_count: prevPost.results[0].comments_count - 1,
-              },
-            ],
-          }));
-    
-          setComments((prevComments) => ({
-            ...prevComments,
-            results: prevComments.results.filter((comment) => comment.id !== id),
-          }));
-        } catch (err) {}
-      };
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPost((prevPost) => ({
+        results: [
+          {
+            ...prevPost.results[0],
+            comments_count: prevPost.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.filter((comment) => comment.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
-    <div>
+    <>
       <hr />
       <Media>
-          <Avatar src={profile_image} />
+        <Avatar src={profile_image} />
         <Media.Body className="align-self-center ml-2">
           <span className={styles.Owner}>{owner}</span>
           <span className={styles.Date}>{updated_at}</span>
-          <p>{content}</p>
+          {showEditForm ? (
+            <CommentEditForm
+              id={id}
+              content={content}
+              profileImage={profile_image}
+              setComments={setComments}
+              setShowEditForm={setShowEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )}
         </Media.Body>
-        {is_owner && (
-          <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
         )}
       </Media>
-    </div>
+    </>
   );
 };
 
