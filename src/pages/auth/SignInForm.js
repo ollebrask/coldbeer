177c5b3c4eee0547e2098b2faa4a88aa705bbeea
Olle Brask <ollstah@gmail.com>
2024-07-10 18:58:12 +0000

    Add semicolon

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -38,7 +38,7 @@ function SignInForm() {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
       setTokenTimestamp(data);
-      history.push("/")
+      history.push("/");
     } catch (err) {
       setErrors(err.response?.data);
     }
